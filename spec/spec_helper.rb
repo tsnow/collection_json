829dc2eff070504d8d4bdc01cb6b1e98b60295c0
Mark Burns <markthedeveloper@gmail.com>
2012-02-25 20:39:02 +0000

    fixed specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@
 # loaded once.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-Dir['./app/models/**/*.rb'].each {|r| require r}
+Dir['./lib/**/*.rb'].each {|r| require r}
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
