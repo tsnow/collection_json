c975ce275c4acfa107d358509e7c13adeb107a40
Mark Burns <markthedeveloper@gmail.com>
2012-02-26 08:29:13 +0000

    starting query templates

--- lib/collection_json/query.rb
+++ lib/collection_json/query.rb
@@ -1,4 +1,52 @@
+require 'active_support/deprecation'
+require 'active_support/concern'
+require 'virtus'
+
 module CollectionJson
   module Query
+    extend ActiveSupport::Concern
+    include Virtus
+
+    def to_json
+      query_template.to_json
+    end
+
+    def query_template
+      {
+        queries:
+        [
+          {
+            href:   self.class.url,
+            rel:    self.class.rel,
+            prompt: self.class.prompt,
+            data:   attributes.map{|k, v| {name: k.to_s, value: v}}
+         }
+        ]
+      }
+    end
+
+    included do
+      class_eval do
+        def self.url *args
+          return @url = args[0] if args.any?
+          @url
+        end
+
+        def self.rel *args
+          return @rel = args[0] if args.any?
+          @rel
+        end
+
+        def self.prompt *args
+          return @prompt = args[0] if args.any?
+          @prompt
+        end
+
+        def self.query
+          yield
+        end
+
+      end
+    end
   end
 end
--- spec/lib/collection_json/query_spec.rb
+++ spec/lib/collection_json/query_spec.rb
@@ -1,7 +1,86 @@
+require File.expand_path('spec/spec_helper')
+
 #1.2. Query Templates
-#Clients that support the Collection+JSON media type SHOULD be able to recognize and parse query templates found within responses. Query templates consist of a data array associated with an href property. The queries array supports query templates.
+#Clients that support the Collection+JSON media type SHOULD be able to
+#recognize and parse query templates found within responses.
+#Query templates consist of a data array associated with an href property. The
+#queries array supports query templates.
 
-#For query templates, the name/value pairs of the data array set are appended to the URI found in the href property associated with the queries array (with a question-mark ["?"] as separator) and this new URI is sent to the processing agent.
+#For query templates, the name/value pairs of the data array set are appended
+#to the URI found in the href property associated with the queries array (with
+#a question-mark ["?"] as separator) and this new URI is sent to the processing
+#agent.
+
+#In the above example, if the user supplied "JSON" for the value property, the
+#user agent would construct the following URI:
+
+#http://example.org/search?search=JSON
+class Book
+  include CollectionJson::Query
+
+  url "http://example.org/search"
+  rel "search"
+  prompt "Enter search string"
+
+  attribute :title,         String
+  attribute :author,        String
+  attribute :publish_date,  DateTime
+  attribute :readers_count, Integer
+end
+
 describe CollectionJson::Query do
+  let(:query_template) do
+    # query template sample
+    {
+      queries:
+      [
+        {
+          href: "http://example.org/search",
+          rel: "search",
+          prompt: "Enter search string",
+          data:
+          [
+            {name: "title",         value: "The long and winding road"},
+            {name: "author",        value: "Jack Smith"},
+            {name: "publish_date",  value: clock_tower_time},
+            {name: "readers_count", value: nil}
+          ]
+        }
+      ]
+    }
+  end
+ let(:query_template_with_count) do
+    # query template sample
+    {
+      queries:
+      [
+        {
+          href: "http://example.org/search",
+          rel: "search",
+          prompt: "Enter search string",
+          data:
+          [
+            {name: "title",         value: "The long and winding road"},
+            {name: "author",        value: "Jack Smith"},
+            {name: "publish_date",  value: clock_tower_time},
+            {name: "readers_count", value: 10}
+          ]
+        }
+      ]
+    }
+  end
 
+  let(:clock_tower_time){DateTime.parse("1955-11-05T20:00:00+09:00")}
+
+  let(:book) do
+    Book.new title: "The long and winding road", author: "Jack Smith",
+      publish_date: clock_tower_time
+  end
+
+  specify { book.query_template.should == query_template }
+
+  specify do
+    book.readers_count=10
+    book.query_template.should == query_template_with_count
+  end
 end
